9323864bd2dc6cfe45ee51f33ea10dbe2a440943
Puneeth M <puneetm12345@gmail.com>
2025-03-06 20:37:20 +0530

    getPicklist demo

---
+++ force-app/main/default/lwc/getPicklistDemo/getPicklistDemo.js
@@ -0,0 +1,39 @@
+import { getObjectInfo, getPicklistValues } from "lightning/uiObjectInfoApi";
+import { LightningElement, wire } from "lwc";
+import ACCOUNT_OBJECT from "@salesforce/schema/Account";
+import ACCOUNT_INDUSTRY from "@salesforce/schema/Account.Industry";
+
+export default class GetPicklistDemo extends LightningElement {
+  value;
+  industrypicklistValues;
+  @wire(getObjectInfo, {
+    objectApiName: ACCOUNT_OBJECT
+  })
+  accProp;
+
+  @wire(getPicklistValues, {
+    recordTypeId: "$accProp.data.defaultRecordTypeId",
+    fieldApiName: ACCOUNT_INDUSTRY
+  })
+  //industrypicklistValues;
+  outputFunction({ data, error }) {
+    if (data) {
+      console.log("Picklist Data", data);
+      let updateObject = { label: "", value: "" };
+      let updatedArray = data.values.map((currItem) => {
+        console.log("currItem.label", currItem.label);
+        console.log("currItem.value", currItem.value);
+        updateObject = { label: currItem.label, value: currItem.value };
+        console.log("updateObject", updateObject);
+        return updateObject;
+      });
+      console.log("updatedArray", updatedArray);
+      this.industrypicklistValues = [...updatedArray];
+    } else if (error) {
+      console.log("Error", error);
+    }
+  }
+  handleChange(event) {
+    this.value = event.target.value;
+  }
+}
